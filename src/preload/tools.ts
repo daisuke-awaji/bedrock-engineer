4e6af56483641b46874355384e94b114441aa7f6
Daisuke Awaji <gee.awa@gmail.com>
2024-10-08 09:43:19 +0900

    fix: npm audit

--- src/preload/tools.ts
+++ src/preload/tools.ts
@@ -4,7 +4,6 @@ import * as path from 'path'
 import { store } from './store'
 import * as diff from 'diff'
 import GitignoreLikeMatcher from './lib/gitignore-like-matcher'
-import * as XLSX from 'xlsx'
 
 export async function createFolder(folderPath: string): Promise<string> {
   try {
@@ -28,23 +27,7 @@ export async function readFiles(filePaths: string[]): Promise<string> {
   try {
     const fileContents = await Promise.all(
       filePaths.map(async (filePath) => {
-        const ext = path.extname(filePath).toLowerCase()
-        let content: string
-
-        if (ext === '.xlsx' || ext === '.xls') {
-          // エクセルファイルの読み込み
-          const workbook = XLSX.readFile(filePath)
-          const sheetNames = workbook.SheetNames
-          content = sheetNames
-            .map((sheetName) => {
-              const sheet = workbook.Sheets[sheetName]
-              return `Sheet: ${sheetName}\n${XLSX.utils.sheet_to_csv(sheet)}\n`
-            })
-            .join('\n')
-        } else {
-          // 通常のテキストファイルの読み込み
-          content = await fs.readFile(filePath, 'utf-8')
-        }
+        const content = await fs.readFile(filePath, 'utf-8')
 
         return { path: filePath, content }
       })
